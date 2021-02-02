04b40af45908c962c896d69d5a409fabb7d127b0
Yash Saini <ysaini@paymentus.com>
2021-02-02 18:59:59 +0530

    Unnecessary code removed

--- todo-list-api-service/src/tasks/interfaces/task.interface.ts
+++
@@ -1,8 +0,0 @@
-import mongoose, { Document } from 'mongoose';
-
-export interface ITask extends Document {
-  title: string;
-  userId: mongoose.Types.ObjectId;
-  status: string;
-  created_date: Date;
-}
